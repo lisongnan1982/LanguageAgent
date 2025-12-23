bacad32d1e90befe16cbb6a4321d45fbd04ed1f6
sunnysnli <sunnysnli@tencent.com>
2025-12-23 15:49:32 +0800

    设置修改

--- server.js
+++ server.js
@@ -634,6 +634,7 @@ app.post('/api/text-to-image', async (req, res) => {
             steps: num_inference_steps || restModelConfig.steps,
             width: width || restModelConfig.defaultWidth,
             height: height || restModelConfig.defaultHeight,
+            disable_safety_checker: true,
             prompt: prompt,
             negative_prompt: negative_prompt || '(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck'
         };
