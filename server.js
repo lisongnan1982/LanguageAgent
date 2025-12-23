d2effbfe7dc7ec6c6b05b6514755ba3687518a5f
sunnysnli <sunnysnli@tencent.com>
2025-12-23 09:53:26 +0800

    删除comments

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/*
- * @Author: sunnysnli sunnysnli@tencent.com
- * @Date: 2025-12-21 16:02:16
- * @LastEditors: sunnysnli sunnysnli@tencent.com
- * @LastEditTime: 2025-12-21 16:21:26
- * @FilePath: \LanguageAgent\server.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
