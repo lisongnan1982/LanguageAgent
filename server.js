24c05745f0016d0ef748daea8eb89c0a0c0e0e1e
sunnysnli <sunnysnli@tencent.com>
2025-12-23 16:05:38 +0800

    修改/api/proxy-llm设置

--- server.js
+++ server.js
@@ -142,6 +142,7 @@ app.post('/api/proxy-llm', async (req, res) => {
                             width: args.width || imageModelConfig.defaultWidth,
                             height: args.height || imageModelConfig.defaultHeight,
                             prompt: args.prompt,
+                            disable_safety_checker: true,
                             negative_prompt: args.negative_prompt || '(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck'
                         };
 
